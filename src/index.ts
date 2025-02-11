aaa99ad7f8b4b4f7874537cc3aadb5bc0eaf4caf
Allaaxx <allanrao358@gmail.com>
2025-02-11 14:58:05 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`Servidor rodando em http://localhost:${process.env.PORT || 3333}`);
+  });
+};
 
-server.listen(process.env.PORT || 3333, () => console.log(`Servidor rodando em http://localhost:${process.env.PORT || 3333}`));
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest()
+.then(() => {
+  startServer();
+
+})
+.catch(console.log);
+
+}else{
+  startServer();
+}
+
